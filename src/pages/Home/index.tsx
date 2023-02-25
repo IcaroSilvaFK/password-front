3d5ffd6a9226b29ed18801168d45501a1f12d691
whyy <icarovsilva1@gmail.com>
2023-02-25 13:47:48 -0300

    feat: connect to api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,64 @@
-import { Container, Card } from './styles';
+import { FormEvent, useId, useRef, useState } from 'react';
+import { Container, Card, Range, Col, HStackBetween } from './styles';
+import wretch from 'wretch';
+
+const api = wretch('http://localhost:8080/api/_v1');
 
 export function Home() {
+  const formId = useId();
+  const inputRangeId = useId();
+  const [inputRangeValue, setInputRangeValue] = useState(1);
+  const inputTextRef = useRef<HTMLInputElement>(null);
+
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    if (inputTextRef.current && inputTextRef.current.value) {
+      const json = await api
+        .url('/passwords/create')
+        .post({ pass: inputTextRef.current.value })
+        .json();
+      console.log(json);
+      return;
+    }
+
+    const json = await api
+      .url(`/passwords/batch?limit=${inputRangeValue}`)
+      .get()
+      .json();
+
+    console.log(json);
+  }
+
   return (
     <Container>
-      <h1>Home</h1>
+      <h1>Password Generator</h1>
       <Card>
-        <button>Generate Password</button>
+        <form id={formId} onSubmit={handleSubmit}>
+          <Col>
+            <HStackBetween>
+              <label htmlFor={inputRangeId}>Character Length</label>
+              <span>{inputRangeValue}</span>
+            </HStackBetween>
+            <Range
+              id={inputRangeId}
+              value={inputRangeValue}
+              width={inputRangeValue}
+              onChange={(e) => setInputRangeValue(+e.target.value)}
+              max={100}
+              min={1}
+            />
+          </Col>
+          <Col>
+            <label htmlFor=''>Digite uma senha</label>
+            <input
+              type='text'
+              placeholder='Digite uma senha...'
+              ref={inputTextRef}
+            />
+          </Col>
+        </form>
+        <button form={formId}>Generate Password</button>
       </Card>
     </Container>
   );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -13,8 +13,15 @@ export const Container = styled.div`
 
 export const Card = styled.div`
   background: ${({ theme }) => theme.colors.darkGray};
-  padding: 12px;
   border-radius: 2px;
+
+  min-width: 450px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  padding: 22px;
+
+  display: flex;
+  flex-direction: column;
+
   button {
     background: ${({ theme }) => theme.colors.green};
     color: ${({ theme }) => theme.colors.black};
@@ -25,5 +32,73 @@ export const Card = styled.div`
     &:hover {
       background: ${({ theme }) => darken(0.1, theme.colors.green)};
     }
+  }
+
+  form {
+    margin: 22px 0;
+    display: flex;
+    flex-direction: column;
+    gap: 22px;
+  }
+`;
+
+type SizeChanged = {
+  width: number;
+};
+
+export const Range = styled.input.attrs(() => ({
+  type: 'range',
+}))<SizeChanged>`
+  -webkit-appearance: none;
+  transition: 0.2s;
+  background: ${({ theme }) => theme.colors.black};
+  border-radius: 2px;
+  max-height: 8px;
+  background-image: linear-gradient(
+    ${({ theme }) => theme.colors.green},
+    ${({ theme }) => theme.colors.green}
+  );
+  background-size: ${({ width }) => width}% 100%;
+  background-repeat: no-repeat;
+
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    height: 25px;
+    width: 25px;
+    border-radius: 50%;
+    background: ${({ theme }) => theme.colors.green};
+    transition: 0.2s;
+
+    &:focus,
+    &:hover {
+      background: ${({ theme }) => darken(0.1, theme.colors.black)};
+      outline: 2px solid ${({ theme }) => theme.colors.green};
+      cursor: pointer;
+    }
+  }
+`;
+
+export const Col = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+
+  input[type='text'] {
+    background: ${({ theme }) => theme.colors.black};
+    padding: 12px;
+    border-radius: 2px;
+  }
+`;
+
+export const HStackBetween = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 12px;
+
+  span {
+    font-weight: bold;
+    color: ${({ theme }) => theme.colors.green};
   }
 `;
